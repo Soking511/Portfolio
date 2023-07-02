0bd92eae75fbfdb031acd594fee4c567dd188da2
Soking511 <youseeftareq5176@gmail.com>
2023-07-02 11:02:35 +0300

    Updated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './Styles.css';
+import Photo from './Photos/Photo.jpg';
 
-const Header = (
-  <header>
-    <h1> Test 1 </h1>
-    <h1> Test 2 </h1>
-    <h1> Test 3 </h1>
-    <h1> Test 4 </h1>
-  </header>
-)
+const Name = 'Youseef Tareq';
+const Department = 'Front-End'
+
+const Header = () => {
+  return (
+    <header class='header'>
+    <nav class="header-right">
+      {/* <label class="Title">Health Scanner</label> */}
+      {/* <img class='profilePhoto' src={Photo} width="45" height="45"></img> */}
+      <a href="#Home">Home</a>
+      <a href="#Services">Services</a>
+      <a href="#Portfolio">Portfolio</a>
+      <a href="#Details">Details</a>
+    </nav>
+    </header>
+  );
+};
+
+const Body = () => {
+  return (
+    <div class='IntroductoryText'>
+      <span id='firstSpan'> Hello, <br></br> My Name is: {Name} </span>
+      <br></br><br></br>
+      <span id='secondSpan'>
+        i have worked as a web {Department} developer for over three <br></br>
+        years, with extensive experience in designing and<br></br>
+        developing attractive and responsive user interfaces using<br></br>
+        HTML, CSS, JavaScript, and popular frameworks such as<br></br>
+        Bootstrap, jQuery, and React.
+      </span>
+    </div>
+  );
+};
+
 
 
 const app = (
   <div>
-    {Header}
+  {<Header/>}
+  {<Body/>}
   </div>
 )
 
